b9ec5f35b502dd23efe95a58327ede751dfbfac7
Manish Kumar <manish.mk0195@gmail.com>
2021-09-23 14:02:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
-function App() {
+import { useState } from "react";
+
+const foodDB = {
+  breakfast: [
+    { name: "Dosa and chutney", rating: "4/5" },
+    { name: "Bread and peanut butter", rating: "3.5/5" },
+    { name: "omlette", rating: "4.5/5" }
+  ],
+
+  lunch: [
+    { name: "Rice, dal and salad", rating: "4/5" },
+    { name: "Mixed veg rice with curd", rating: "4.5/5" }
+  ],
+  dinner: [
+    { name: "Chapati and vegetables", rating: "4.5/5" },
+    { name: "Salad", rating: "5/5" }
+  ]
+};
+
+export default function App() {
+  const [selectedCuisine, setCuisine] = useState("dinner");
+  function cuisineClickHandler(cuisine) {
+    setCuisine(cuisine);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> 🥗 goodfoods </h1>
+      <p style={{ fontSize: "smaller" }}> These are my food recommendations for your good health. </p>
+      <div>
+        {Object.keys(foodDB).map((cuisine) => (
+          <button onClick={() => cuisineClickHandler(cuisine)} style={{ 
+            cursor: "pointer", 
+            background: "#E5E7EB", 
+            borderRadius: "0.5rem", 
+            padding: "0.5rem  1rem", 
+            border: "1px solid black", 
+            margin: "1rem 0.3rem"
+            }}
+          >
+            {cuisine}
+          </button>
+        ))}
+      </div>
+      <hr />
+      <div style={{ textAlign: "left" }}>
+        <ul style={{ paddingInlineStart: "0" }}>
+          {foodDB[selectedCuisine].map((food) => (
+            <li
+              key={food.name}
+              style={{
+                listStyle: "none",
+                padding: "1rem",
+                border: "1px solid #D1D5DB",
+                width: "70%",
+                margin: "1rem 0rem",
+                borderRadius: "0.5rem"
+              }}
+            >
+              <div style={{ fontSize: "larger" }}> {food.name} </div>
+              <div style={{ fontSize: "smaller" }}> {food.rating} </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
-
-export default App;
