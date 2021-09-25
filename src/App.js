ea4f63e3e5d6bba5ebf204f7c55cc6c165d156ed
Manish Kumar <manish.mk0195@gmail.com>
2021-09-25 11:41:26 +0530

    review updates

--- src/App.js
+++ src/App.js
@@ -6,23 +6,23 @@ import { useState } from "react";
 
 const foodDB = {
   breakfast: [
-    { name: "Dosa and chutney", rating: "4/5" },
-    { name: "Bread and peanut butter", rating: "3.5/5" },
-    { name: "omlette", rating: "4.5/5" }
+    { name: "Dosa and chutney", description: "It is one of easiest food to cook and is one of healthiest breakfast.", rating: "4/5" },
+    { name: "Bread and peanut butter", description: "This doesn't require any cooking. so, it can be eaten with very less effort and provides great nutrition.", rating: "3.5/5" },
+    { name: "omlette", description: "This is one of my favorite foods. It takes very less time to cook and is tasty. You can add vegetables to it.", rating: "4.5/5" }
   ],
 
   lunch: [
-    { name: "Rice, dal and salad", rating: "4/5" },
-    { name: "Mixed veg rice with curd", rating: "4.5/5" }
+    { name: "Rice, dal and salad", description: "One of the most eaten food by indians. We can call it National luch of India. ", rating: "4/5" },
+    { name: "Mixed veg rice with curd", description: "You can experiment with rice and add some vegetables to it so that it becomes bit healthy and nutritious.", rating: "4.5/5" }
   ],
   dinner: [
-    { name: "Chapati and vegetables", rating: "4.5/5" },
-    { name: "Salad", rating: "5/5" }
+    { name: "Chapati and vegetables", description: "You should eat light and early at night so that you can sleep properly and feel fresh.", rating: "4.5/5" },
+    { name: "Salad", description: "This is the best meal anyone can have for dinner. This is healthy and nutritious and doesn't take much time to cook.",rating: "5/5" }
   ]
 };
 
 export default function App() {
-  const [selectedCuisine, setCuisine] = useState("dinner");
+  const [selectedCuisine, setCuisine] = useState("breakfast");
   function cuisineClickHandler(cuisine) {
     setCuisine(cuisine);
   }
@@ -61,6 +61,7 @@ export default function App() {
               }}
             >
               <div style={{ fontSize: "larger" }}> {food.name} </div>
+              <div style={{ fontSize: "smaller" }}> {food.description} </div>
               <div style={{ fontSize: "smaller" }}> {food.rating} </div>
             </li>
           ))}
